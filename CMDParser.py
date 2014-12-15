f41cccb2336974a4836156fd954ada891e7be3ec
Michal Rynduch <michalrynduch@gmail.com>
2014-12-15 23:01:56 +0100

    Parser updated.

--- CMDParser.py
+++ CMDParser.py
@@ -4,19 +4,16 @@ from argparse import ArgumentParser
 def create_parser():
     parser = ArgumentParser()
 
-    parser.add_argument("-fn", "--file_name", dest="file_name",
-                        help="filename.* - csv and html are supported extensions", type=str, required=True)
+    parser.add_argument("-b", "--biased_dice", dest="biased_dice",
+                        help="biased dice", type=str, required=True)
 
-    parser.add_argument("-f", "--folder", dest="folder_path",
-                        help="test set folder", type=str, default=r'Root\ContinuousTests\BasicScope')
+    parser.add_argument("-pfb", "--fair_to_biased", dest="fair_to_biased",
+                        help="probability from fair to biased", type=float, required=True)
 
-    parser.add_argument("-lr", "--last_runs", dest="last_runs",
-                        help="number of runs", type=int, default=1)
+    parser.add_argument("-pbf", "--biased_to_fair", dest="biased_to_fair",
+                        help="probability from fair to biased", type=float, required=True)
 
-    parser.add_argument("-ts", "--test_set_name", dest="test_set_name",
-                        help="test set name", type=str, default=None)
-
-    parser.add_argument("-p", "--project", dest="project_name",
-                        help="QC project name", type=str, default="KondorPlus")
+    parser.add_argument("-t", "--algorithm_type", dest="algorithm_type",
+                        help="algorithm type", type=str, required=True)
 
     return parser
--- __init__.py
+++ __init__.py
@@ -1,34 +1,40 @@
 from collections import __main__
 import logging
-
-################################################# LOGGER CONFIGURATION #################################################
 from CMDParser import create_parser
 
-LOG_FILE     = 'casino.log'
-FILEMODE     = 'a'
-MAX_BYTES    = 1048576
-BACKUP_COUNT = 20
-LEVEL        = logging.DEBUG
-
-FORMAT = '%(asctime)-15s %(levelname)8s %(process)5d %(thread)6d %(name)-25s %(filename)20s:%(lineno)-6s %(message)s'
-
-root_logger_file_handler = logging.handlers.RotatingFileHandler(filename=LOG_FILE,
-                                                                mode=FILEMODE,
-                                                                maxBytes=MAX_BYTES,
-                                                                backupCount=BACKUP_COUNT)
-root_logger_formatter = logging.Formatter(fmt=FORMAT, datefmt=None)
-root_logger_file_handler.setFormatter(root_logger_formatter)
-
-root_logger = logging.getLogger()
-root_logger.addHandler(root_logger_file_handler)
-root_logger.setLevel(LEVEL)
-########################################################################################################################
+logger = logging.getLogger(__name__)
+# ################################################# LOGGER CONFIGURATION #################################################
+#
+# LOG_FILE     = 'casino.log'
+# FILEMODE     = 'a'
+# MAX_BYTES    = 1048576
+# BACKUP_COUNT = 20
+# LEVEL        = logging.DEBUG
+#
+# FORMAT = '%(asctime)-15s %(levelname)8s %(process)5d %(thread)6d %(name)-25s %(filename)20s:%(lineno)-6s %(message)s'
+#
+# root_logger_file_handler = logging._handlers.RotatingFileHandler(filename=LOG_FILE,
+#                                                                 mode=FILEMODE,
+#                                                                 maxBytes=MAX_BYTES,
+#                                                                 backupCount=BACKUP_COUNT)
+# root_logger_formatter = logging.Formatter(fmt=FORMAT, datefmt=None)
+# root_logger_file_handler.setFormatter(root_logger_formatter)
+#
+# root_logger = logging.getLogger()
+# root_logger.addHandler(root_logger_file_handler)
+# root_logger.setLevel(LEVEL)
+# ########################################################################################################################
 
 
 def main():
     parser = create_parser()
 
     args = parser.parse_args()
+
+    if args.algorithm_type == "verbati":
+        print(args)
+    elif args.algorithm_type == "aposteriori":
+        print(args)
 
 if __name__ == '__main__':
     main()
